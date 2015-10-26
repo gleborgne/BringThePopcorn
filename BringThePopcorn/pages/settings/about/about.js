f6b57f18f7d3d764efe10a1f47aa2f1a8c1c66a6
gleborgne <gleborgne@mcnext.com>
2015-10-26 10:28:21 +0100

    evolve appinsight integration

--- BringThePopcorn/pages/settings/about/about.js
+++ BringThePopcorn/pages/settings/about/about.js
@@ -3,9 +3,18 @@
 
     WinJS.UI.Pages.define("/pages/settings/about/about.html", {
         processed: function (element, options) {
+            var ctrl = this;
             var version = Windows.ApplicationModel.Package.current.id.version;
             this.versionNumber.innerText = version.major + '.' + version.minor + '.' + version.build + "." + version.revision;
-            //throw new Error("oula la la la")
+            //if (ctrl.runerror) {
+            //    ctrl.runerror.onclick = function () {
+            //        ctrl.throwError();
+            //    };
+            //}
+        },
+
+        throwError: function () {
+            throw new Error("test oups there was an error");
         }
     });
 })();
